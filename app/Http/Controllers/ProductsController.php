6c992423cb5657a365ae2f2550f99ceeeb94d3b9
vagrant <vagrant@homestead>
2019-09-09 09:39:05 +0000

    推荐相似商品

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,6 +10,8 @@ use App\Models\Category;
 use App\Services\CategoryService;
 use Illuminate\Pagination\LengthAwarePaginator;
 use App\SearchBuilders\ProductSearchBuilder;
+use App\Services\ProductService;
+
 
 class ProductsController extends Controller
 {
@@ -219,12 +221,17 @@ class ProductsController extends Controller
 
         // 通过 collect 函数将返回结果转为集合，并通过集合的 pluck 方法取到返回的商品 ID 数组
         $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
+
+        /*
         // 通过 whereIn 方法从数据库中读取商品数据
         $products = Product::query()
             ->whereIn('id', $productIds)
             // orderByRaw 可以让我们用原生的 SQL 来给查询结果排序
             ->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $productIds)))
-            ->get();
+            ->get();*/
+
+        $products = Product::query()->byIds($productIds)->get();
+
         // 返回一个 LengthAwarePaginator 对象
         $pager = new LengthAwarePaginator($products, $result['hits']['total'], $perPage, $page, [
             'path' => route('products.index', false), // 手动构建分页的 url
@@ -305,7 +312,79 @@ class ProductsController extends Controller
         ]);
     }
 
-    public function show(Product $product, Request $request)
+
+    // 利用 Laravel 的自动注入来初始化 ProductService 对象
+    public function show(Product $product, Request $request, ProductService $service)
+    {
+        if (!$product->on_sale) {
+            throw new InvalidRequestException('商品未上架');
+        }
+
+        $favored = false;
+        // 用户未登录时返回的是 null，已登录时返回的是对应的用户对象
+        if($user = $request->user()) {
+            // 从当前用户已收藏的商品中搜索 id 为当前商品 id 的商品
+            // boolval() 函数用于把值转为布尔值
+            $favored = boolval($user->favoriteProducts()->find($product->id));
+        }
+
+        $reviews = OrderItem::query()
+            ->with(['order.user', 'productSku']) // 预先加载关联关系
+            ->where('product_id', $product->id)
+            ->whereNotNull('reviewed_at') // 筛选出已评价的
+            ->orderBy('reviewed_at', 'desc') // 按评价时间倒序
+            ->limit(10) // 取出 10 条
+            ->get();
+
+
+
+        /*
+        // 创建一个查询构造器，只搜索上架的商品，取搜索结果的前 4 个商品
+        $builder = (new ProductSearchBuilder())->onSale()->paginate(4, 1);
+        // 遍历当前商品的属性
+        foreach ($product->properties as $property) {
+            // 添加到 should 条件中
+            $builder->propertyFilter($property->name, $property->value, 'should');
+        }
+        // 设置最少匹配一半属性
+        $builder->minShouldMatch(ceil(count($product->properties) / 2));
+        $params = $builder->getParams();
+        // 同时将当前商品的 ID 排除
+        $params['body']['query']['bool']['must_not'] = [['term' => ['_id' => $product->id]]];
+        // 搜索
+        $result = app('es')->search($params);
+        $similarProductIds = collect($result['hits']['hits'])->pluck('_id')->all();
+        // 根据 Elasticsearch 搜索出来的商品 ID 从数据库中读取商品数据
+        $similarProducts   = Product::query()
+            ->whereIn('id', $similarProductIds)
+            ->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $similarProductIds)))
+            ->get();
+
+        */
+
+        $similarProductIds = $service->getSimilarProductIds($product, 4);
+
+        /*
+        $similarProducts   = Product::query()
+            ->whereIn('id', $similarProductIds)
+            ->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $similarProductIds)))
+            ->get();
+        */
+        
+        $similarProducts   = Product::query()->byIds($similarProductIds)->get();
+
+
+        // 最后别忘了注入到模板中
+        return view('products.show', [
+            'product' => $product,
+            'favored' => $favored,
+            'reviews' => $reviews,
+            'similar' => $similarProducts,
+        ]);
+    }
+
+
+    public function show_old(Product $product, Request $request)
     {
         if (!$product->on_sale) {
             throw new InvalidRequestException('商品未上架');
